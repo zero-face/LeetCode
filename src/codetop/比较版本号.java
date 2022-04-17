5ee818cc04c4496fa6adb5671da6e23f0a7e821c
zero <1444171773@qq.com>
2022-04-17 21:06:02 +0800

    feat(global): 电话号码全排列

---
+++ src/codetop/比较版本号.java
@@ -0,0 +1,31 @@
+package codetop;
+
+/**
+ * @author Zero
+ * @date 2022/3/28 9:42
+ * @description
+ * @since 1.8
+ **/
+public class 比较版本号 {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        for (int i = 0; i < v1.length || i < v2.length; ++i) {
+            int x = 0, y = 0;
+            if (i < v1.length) {
+                x = Integer.parseInt(v1[i]);
+            }
+            if (i < v2.length) {
+                y = Integer.parseInt(v2[i]);
+            }
+            if (x > y) {
+                return 1;
+            }
+            if (x < y) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
